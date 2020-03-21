2d8d6d35f964265cf8b75de8f4ef2a030900e3a0
riyadhul <riyadhul@gmail.com>
2020-03-21 13:37:10 +0700

    del

--- include/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>q2GVo2Sm');
-
-$data['Riyadhul'] = array ('1'=>'Riyadhul!75.88.57.1','Riyadhul@|@admin','Riyadhul#|#q2GVo2Sm','Riyadhul%HotSpot Warkop','Riyadhul^warkop.net','Riyadhul&Rp','Riyadhul*10','Riyadhul(1','Riyadhul)','Riyadhul=disable','Riyadhul@!@enable');
