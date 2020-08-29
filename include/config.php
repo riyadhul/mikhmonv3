18fef2a02f8e518d0d3a2ef7e03a8aeeee6572a3
riyadhul <30875924+riyadhul@users.noreply.github.com>
2020-08-29 10:02:44 +0700

    Add files via upload

--- include/config.php
+++ include/config.php
@@ -1,9 +1,8 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>q2GVo2Sm');
-
-$data['Riyadhul'] = array ('1'=>'Riyadhul!75.88.57.1','Riyadhul@|@admin','Riyadhul#|#q2GVo2SmpWWVoGKgaw==','Riyadhul%HotSpot Warkop','Riyadhul^warkop.net','Riyadhul&Rp','Riyadhul*10','Riyadhul(1','Riyadhul)','Riyadhul=disable','Riyadhul@!@enable');
-
-$data['Vertikal.Net'] = array ('1'=>'Vertikal.Net!75.88.57.1','Vertikal.Net@|@admin','Vertikal.Net#|#q2GVo2Sm','Vertikal.Net%Vertikal Net','Vertikal.Net^Vertikal.Ag.Net','Vertikal.Net&Rp','Vertikal.Net*10','Vertikal.Net(1','Vertikal.Net)','Vertikal.Net=disable','Vertikal.Net@!@disable');
+$data['Riyadhul'] = array ('1'=>'Riyadhul!192.168.168.1','Riyadhul@|@admin','Riyadhul#|#q2GVo2SmpWWVoGKgaw==','Riyadhul%HotSpot-Warkop','Riyadhul^warkop.net','Riyadhul&Rp','Riyadhul*10','Riyadhul(5','Riyadhul)','Riyadhul=disable','Riyadhul@!@enable');
 $data['Arifin'] = array ('1'=>'Arifin!id-3.hostddns.us:9291','Arifin@|@admin','Arifin#|#q2GVo2SmpWWVoGKgaw==','Arifin%Alfatih Net','Arifin^alfatih.net','Arifin&Rp','Arifin*10','Arifin(6','Arifin)','Arifin=disable','Arifin@!@enable');
-$data['Keandra21'] = array ('1'=>'Keandra21!192.168.168.1','Keandra21@|@admin','Keandra21#|#qaiXqqWrrZqhqA==','Keandra21%Keandra21 wifi','Keandra21^Keandra21.net','Keandra21&Rp','Keandra21*10','Keandra21(1','Keandra21)','Keandra21=disable','Keandra21@!@disable');
+$data['Lais'] = array ('1'=>'Lais!id-3.hostddns.us:2975','Lais@|@ryd-andi','Lais#|#q2GVo2SmpWWVoGKgaw==','Lais%HotSpot-LAIS','Lais^lais.net','Lais&Rp','Lais*10','Lais(5','Lais)','Lais=disable','Lais@!@enable');
+$data['Akyas'] = array ('1'=>'Akyas!id-5.hostddns.us:8787','Akyas@|@ryd-akyas','Akyas#|#q2GVo2SmpWWVoGKgaw==','Akyas%Hotspot Akyas','Akyas^akyas.net','Akyas&Rp','Akyas*10','Akyas(6','Akyas)','Akyas=disable','Akyas@!@enable');
+$data['Fadil'] = array ('1'=>'Fadil!id-2.hostddns.us:3326','Fadil@|@admin','Fadil#|#q2GVo2SmpWWVoGKgaw==','Fadil%Asik-Murah.Net','Fadil^murah.net','Fadil&Rp','Fadil*10','Fadil(6','Fadil)','Fadil=disable','Fadil@!@enable');
